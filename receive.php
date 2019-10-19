eb1677a98422510aaf0284404ba7e6416dc5dcef
Ricky Shin <richyongshin@gmail.com>
2019-10-19 19:44:09 -0400

    Using php include func
    To shorten code :+1:

--- receive.php
+++ receive.php
@@ -1,47 +1,7 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>JoyToys</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="/joytoys/css/bootstrap.min.css">
-  </head>
-
-  <body>
-
-    <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills float-right">
-            <li class="nav-item">
-              <a class="nav-link" href="/joytoys/index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/joytoys/our-mission.php">Our Mission</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="/joytoys/receive.php">Receive a Gift</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="/joytoys/buy.php">Buy a Gift</a>
-            </li>
-
-
-            <li class="nav-item">
-              <a class="nav-link" href="/donate.php">Donate</a>
-            </li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">JoyToys</h3>
-      </div>
-
+<?php
+    require_once('head.php');
+    require_once('nav.php');
+?>
       <div class="jumbotron">
         <p class="lead">Every child deserves a gift regardless of circumstances.</br>
                         Fill out the form below for a chance to receive a gift for the Holidays.
