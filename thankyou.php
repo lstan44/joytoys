f18abb9ccbffd775d4c2c9c24c299ac219e4de8b
Stanley Lalanne <stanleefit@icloud.com>
2019-10-19 19:57:03 -0400

    thankyou.php works

--- thankyou.php
+++ thankyou.php
@@ -2,4 +2,13 @@
     require_once('head.php');
     require_once('nav.php');
 
+    echo '
+          <div>
+              <h1>THANK YOU FOR REGISTERING </h1>
+              <p> We will email you regarding your gifts soon! </p>
+
+          </div>
+
+
+    '
  ?>
